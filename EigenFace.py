754c11d094c5dea030db2cf444f3368a74254fe3
haruhi-sudo <59717968+haruhi-sudo@users.noreply.github.com>
2021-05-21 15:12:15 +0800

    second commit

--- EigenFace.py
+++ EigenFace.py
@@ -1,102 +1,53 @@
 import numpy as np
-import PIL.Image as Image
-import os
-
-def centralization(images, d=320*243):
-    '''
-        images是图片集合（矩阵集合）
-        d是图片拉伸后的维度
-        输出中心化后的矩阵central_vectors
-    '''
-    n = len(images) # n是图片个数
-    central_vectors = np.zeros([d, n])
-    for i in range(n):
-        images[i] = images[i].flatten()
-        central_vectors[:, i] = images[i]
-
-    row_mean = np.mean(central_vectors, axis=1).reshape(d, 1)
-    central_vectors = central_vectors - row_mean
-    return central_vectors, row_mean.reshape(d)
-
-def PCA(W, num_components = 20):
-    [xshape, yshape] = W.shape
-    if (xshape <= yshape):
-        cov_matrix = np.dot(W, W.T)
-        [eigenvalues, eigenvectors] = np.linalg.eigh(cov_matrix)
-    else:
-        cov_matrix = np.dot(W.T, W)
-        [eigenvalues, eigenvectors] = np.linalg.eigh(cov_matrix)
-        eigenvectors = np.dot(W, eigenvectors)
-        for i in range(yshape):
-            eigenvectors[:, i] = eigenvectors[:, i] / np.linalg.norm(eigenvectors[:, i])
-
-    # 排序特征值
-    idx = np.argsort(-eigenvalues)
-    eigenvalues = eigenvalues[idx]
-    eigenvectors = eigenvectors[: , idx]
-
-    return eigenvalues[0:num_components], eigenvectors[:,0:num_components]
-
-
-def load_images(path, height = 320, width = 243):
-    files = os.listdir(path)
-    ims = []
-    labels = []
-    for filename in files:
-        try:
-            im = Image.open(os.path.join(path, filename)).\
-                resize((height, width), Image.ANTIALIAS).convert("L")
-            ims.append(np.asarray(im, dtype=np.uint8))
-            labels.append(filename[7:9])
-        except:
-            continue
-    return ims, labels
-
-# 用特征向量重构X
-def reconstrcut(eigenvectors, X):
-    return np.dot(np.dot(X, eigenvectors),eigenvectors.T) 
+from utils import centralization, PCA, load_images, reconstrcut
 
 
 def predict(path, eigen_dic, meanface_dic):
     images, labels = load_images(path)
     predict_result = []
     for image in images:
+        
         image = image.flatten()
         tmp = []
+
+        # 用所有类别的特征向量重构该图片
         for key in eigen_dic:
             image_tmp = image - meanface_dic[key]
             image_rec = reconstrcut(eigen_dic[key][1], image_tmp)
             distance = np.sqrt(np.sum((image_rec - image_tmp)**2))
             tmp.append(distance)
         
-        idx = np.argmin(tmp)
+        idx = np.argmin(tmp) # 选出重构误差最小的
         label = list(eigen_dic.keys())[idx]
         predict_result.append(label)
 
-    return predict_result
+    correct = 0
+    for i in range(len(predict_result)):
+        if(labels[i] == predict_result[i]):
+            correct = correct + 1
+    
+    print('预测正确率：{}'.format(correct / len(predict_result)))
 
 
-# B = [np.array([[1,3],[6,4]]), np.array([[2,6],[5,8]])]
-# A = centralization(B, 4, 2)
-# a, b = PCA(A, 1)
-train_path = './yalefaces/train'
-test_path = './yalefaces/test'
-images, labels = load_images(train_path)
+if __name__ == '__main__':
+    train_path = './yalefaces/train'
+    test_path = './yalefaces/test'
+    images, labels = load_images(train_path)
 
-image_dic = {}
-eigen_dic = {}
-meanface_dic = {}
-for i in range(len(labels)):
-    if i == 0 or labels[i] != labels[i-1]:
-        image_dic[labels[i]] = [images[i]]
-    else:
-        image_dic[labels[i]].append(images[i])
+    image_dic = {}
+    eigen_dic = {} # 特征脸字典
+    meanface_dic = {} # 平均脸字典
 
-for key in image_dic:
-    W, row_mean = centralization(image_dic[key])
-    eigenvalues, eigenvectors = PCA(W)
-    eigen_dic[key] = [eigenvalues, eigenvectors]
-    meanface_dic[key] = row_mean
+    for i in range(len(labels)):
+        if i == 0 or labels[i] != labels[i-1]:
+            image_dic[labels[i]] = [images[i]]
+        else:
+            image_dic[labels[i]].append(images[i])
 
-predict_result = predict(test_path, eigen_dic, meanface_dic)
-print(' ')
+    for key in image_dic:
+        W, mean_face = centralization(image_dic[key])
+        eigenvalues, eigenvectors = PCA(W)
+        eigen_dic[key] = [eigenvalues, eigenvectors]
+        meanface_dic[key] = mean_face
+
+    predict(test_path, eigen_dic, meanface_dic)
---
+++ utils.py
@@ -0,0 +1,70 @@
+import numpy as np
+import PIL.Image as Image
+import os
+
+def centralization(images, d=320*243):
+    '''
+        images是图片集合（矩阵集合）
+        d是图片拉伸后的维度
+        输出中心化后的矩阵central_vectors和平均值
+    '''
+    n = len(images) # n是图片个数
+    central_vectors = np.zeros([d, n])
+    for i in range(n):
+        images[i] = images[i].flatten()
+        central_vectors[:, i] = images[i]
+
+    row_mean = np.mean(central_vectors, axis=1).reshape(d, 1)
+    central_vectors = central_vectors - row_mean
+    return central_vectors, row_mean.reshape(d)
+
+
+def PCA(W):
+    [xshape, yshape] = W.shape
+    if (xshape <= yshape):
+        cov_matrix = np.dot(W, W.T)
+        [eigenvalues, eigenvectors] = np.linalg.eigh(cov_matrix)
+    else:
+        cov_matrix = np.dot(W.T, W)
+        [eigenvalues, eigenvectors] = np.linalg.eigh(cov_matrix)
+        eigenvectors = np.dot(W, eigenvectors)
+        for i in range(yshape):
+            eigenvectors[:, i] = eigenvectors[:, i] / np.linalg.norm(eigenvectors[:, i])
+
+    # 排序特征值
+    idx = np.argsort(-eigenvalues)
+    eigenvalues = eigenvalues[idx]
+    eigenvectors = eigenvectors[: , idx]
+
+    eigenvalues_sum = np.sum(eigenvalues)
+    tmp = 0
+    num_components = 0
+    # 选择合适的主成分数量
+    for i in range(yshape):
+        if(tmp / eigenvalues_sum > 0.95):
+            break
+        else:
+            tmp = tmp + eigenvalues[i]
+            num_components = num_components + 1
+
+    return eigenvalues[0:num_components], eigenvectors[:,0:num_components]
+
+
+# 用特征向量重构X
+def reconstrcut(eigenvectors, X):
+    return np.dot(np.dot(X, eigenvectors), eigenvectors.T) 
+
+
+def load_images(path, height = 320, width = 243):
+    files = os.listdir(path)
+    ims = []
+    labels = []
+    for filename in files:
+        try:
+            im = Image.open(os.path.join(path, filename)).\
+                resize((height, width), Image.ANTIALIAS).convert("L")
+            ims.append(np.asarray(im, dtype=np.uint8))
+            labels.append(filename[7:9])
+        except:
+            continue
+    return ims, labels
